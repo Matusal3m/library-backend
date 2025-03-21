cf03f6be81ab01b83de60328184f40f152c890e4
Matusal3m <matusalemdedeus@gmail.com>
2025-03-20 23:02:50 -0300

    add controller base class

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -7,8 +7,8 @@ use Http\Response;
 abstract class Controller
 {
     abstract public function create(Request $request, Response $response): void;
-    abstract public function update(Request $request, Response $response): void;
-    abstract public function getById(Request $request, Response $response): void;
+    abstract public function update(Request $request, Response $response, array $id): void;
+    abstract public function getById(Request $request, Response $response, array $id): void;
     abstract public function getAll(Request $request, Response $response): void;
-    abstract public function delete(Request $request, Response $response): void;
+    abstract public function delete(Request $request, Response $response, array $id): void;
 }
