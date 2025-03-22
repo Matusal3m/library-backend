47393cce60fbe1f073c5d2f4cb8f6773c6270d49
Matusal3m <matusalemdedeus@gmail.com>
2025-03-21 22:33:43 -0300

    removed dice initialization

--- public/index.php
+++ public/index.php
@@ -1,18 +1,11 @@
 <?php
 
-use Dice\Dice;
 use Http\Dispatcher;
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // load api routes
-require __DIR__ . '/../App/Routes/api.php';
-
-$dice = new Dice();
-
-$dice->addRule('Database', [
-    'shared' => true,
-]);
+require_once __DIR__ . '/../App/Routes/api.php';
 
 // handle request
 $path       = $_SERVER['REQUEST_URI'];
